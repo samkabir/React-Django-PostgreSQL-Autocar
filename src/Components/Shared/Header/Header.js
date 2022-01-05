187b773068fdd82f790afd6cecc0ebc8795c71c8
SamiulKabir <samkabir26@gmail.com>
2022-01-05 17:24:09 +0600

    Header Part added

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -3,140 +3,67 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+import { Link } from 'react-router-dom';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import SupportIcon from '@mui/icons-material/Support';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GoogleIcon from '@mui/icons-material/Google';
+import logo from '../../../images/logo-black3.png';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Header = () => {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-  
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
   
     return (
         <div>
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                    >
-                        LOGO
-                    </Typography>
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleOpenNavMenu}
-                        color="inherit"
-                        >
-                        <MenuIcon />
-                        </IconButton>
-                        <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorElNav}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        open={Boolean(anchorElNav)}
-                        onClose={handleCloseNavMenu}
-                        sx={{
-                            display: { xs: 'block', md: 'none' },
-                        }}
-                        >
-                        {pages.map((page) => (
-                            <MenuItem key={page} onClick={handleCloseNavMenu}>
-                            <Typography textAlign="center">{page}</Typography>
-                            </MenuItem>
-                        ))}
-                        </Menu>
-                    </Box>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-                    >
-                        LOGO
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                        <Button
-                            key={page}
-                            onClick={handleCloseNavMenu}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                        >
-                            {page}
-                        </Button>
-                        ))}
-                    </Box>
-
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                        </IconButton>
-                        </Tooltip>
-                        <Menu
-                        sx={{ mt: '45px' }}
-                        id="menu-appbar"
-                        anchorEl={anchorElUser}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={Boolean(anchorElUser)}
-                        onClose={handleCloseUserMenu}
-                        >
-                        {settings.map((setting) => (
-                            <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                            <Typography textAlign="center">{setting}</Typography>
-                            </MenuItem>
-                        ))}
-                        </Menu>
-                    </Box>
+            <Box sx={{ flexGrow: 1  }}>
+                <AppBar style={{ background: '#303338' }} position="static">
+                    <Toolbar>
+                        <Typography sx={{ flexGrow: 1 }}>
+                            <Link style={{textDecoration:'none', color:'white' }} to="/">
+                                <LocalPhoneIcon sx={{color: '#d8721c' }} /> <Typography sx={{color:'white', mr: 3 }} variant="overline" > + 91 123 456 7890 </Typography>
+                            </Link>
+                            <Link style={{textDecoration:'none', color:'white'}} to="/">
+                                <SupportIcon sx={{color: '#d8721c' }} /> <Typography sx={{color:'white', mr: 3}} variant="overline" >  Certified ISO 9001 : 2008 </Typography>
+                            </Link>
+                            <Link style={{textDecoration:'none', color:'white'}} to="/">
+                                <SupportIcon sx={{color: '#d8721c' }} /> <Typography sx={{color:'white', mr: 3}} variant="overline" >  Mon-Fri: 8am - 7pm </Typography>
+                            </Link>
+                        </Typography>
+                        <Box>
+                            <FacebookIcon sx={{mx:1}} /> <TwitterIcon sx={{mx:1}}/><LinkedInIcon sx={{mx:1}}/> < GoogleIcon sx={{mx:1}} />
+                        </Box>
                     </Toolbar>
-                </Container>
-            </AppBar>
+                </AppBar>   
+            </Box>
+            <Box sx={{ flexGrow: 1, pb: 2 }}>
+                <AppBar style={{ background: '#ffffff' }} position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            <Link style={{textDecoration:'none', color:'black'}} to="/">
+                                <img src={logo} alt="Company LOGO" width="200" height="50" /> 
+                            </Link>
+                        </Typography>
+                        <Box>
+                            <Link style={{textDecoration:'none', color:'black'}} to="/"><Button color="inherit">HOME</Button></Link>
+                            <Link style={{textDecoration:'none', color:'black'}} to="/"><Button color="inherit">FEATURES</Button></Link>
+                            <Link style={{textDecoration:'none', color:'black'}} to="/"><Button color="inherit">PAGES</Button></Link>
+                            <Link style={{textDecoration:'none', color:'black'}} to="/"><Button color="inherit">SHOP</Button></Link>
+                            <Link style={{textDecoration:'none', color:'black'}} to="/"><Button color="inherit">BLOG</Button></Link>
+                            <Link style={{textDecoration:'none', color:'black'}} to="/"><Button color="inherit">SHORTCODES</Button></Link>
+                            <Link style={{textDecoration:'none', color:'black'}} to="/"><Button color="inherit">CONTACT US</Button></Link>
+                            <SearchIcon sx={{color: '#000000', mx: 2 }} />
+                            <ShoppingBagIcon sx={{color: '#000000', mx: 2 }} />
+                            <Link style={{textDecoration:'none', color:'white'}} to="/"><Button sx={{ backgroundColor:'#f68220', p:3}} color="inherit">GET A QUOTE</Button></Link>
+                        </Box>
+                    </Toolbar>
+                </AppBar>   
+            </Box>
         </div>
     );
 };
