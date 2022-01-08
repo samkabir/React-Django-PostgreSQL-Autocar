02033b65db6f39472245a94da191f5bf33a79eb3
SamiulKabir <samkabir26@gmail.com>
2022-01-08 14:27:56 +0600

    added a few stuff

---
+++ src/Pages/Home/GetAQuote/GetAQuote.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const GetAQuote = () => {
+    return (
+        <div>
+            <Container >
+            <Grid container spacing={0}>
+                <Grid item xs={12} md={6}>
+                    <h2>This is Title and Paragraph</h2>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <h2>This is the image section</h2>
+                </Grid>
+            </Grid>
+            </Container>
+        </div>
+    );
+};
+
+export default GetAQuote;
