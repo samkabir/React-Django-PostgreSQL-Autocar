b6501ea8f9486dea42dc34e3359a5d542ebb1653
SamiulKabir <samkabir26@gmail.com>
2022-01-10 11:11:13 +0600

    added a new folder named Emailand Support

---
+++ src/Pages/Home/EmailandSupport/EmailandSupport.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmailandSupport = () => {
+    return (
+        <div>
+            <h2>This the email and support section</h2>
+        </div>
+    );
+};
+
+export default EmailandSupport;
