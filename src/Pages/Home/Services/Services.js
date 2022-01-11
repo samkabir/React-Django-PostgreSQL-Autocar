2b1506d08c28e4a03241de93824d33caac0840c6
SamiulKabir <samkabir26@gmail.com>
2022-01-11 17:13:21 +0600

    created a json file and displayed the data in services section

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -2,21 +2,18 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import styled from "styled-components";
-import image1 from '../../../images/pic1_002.jpg';
-import image2 from '../../../images/pic2_003.jpg';
-import image3 from '../../../images/pic3_003.jpg';
-import image4 from '../../../images/pic4.jpg';
-import image5 from '../../../images/pic5.jpg';
-import image6 from '../../../images/pic6.jpg';
-import breakpoint from '../../Shared/Breakpoints/Breakpoints';
+import { useEffect, useState } from 'react';
+import Service from './Service/Service';
 
 const Services = () => {
-    const [services, setServices] = useState([])
+    const [services, setServices] = useState([]);
     useEffect( () => {
-        fetch('services.JSON')
+        fetch('data.json')
         .then(res => res.json())
-        .then(data => setServices(data))
+        .then(data => setServices(data.allservices))
     },[]);
+
+
 
     return (
         <Wrapper>
@@ -24,7 +21,12 @@ const Services = () => {
                 <Title>OUR SERVICES</Title>
                 <Description>There are many variations of passages of Lorem Ipsum typesetting industry has been the industry's <br /> standard dummy text ever since the been when an unknown printer. </Description>
                 <Grid container spacing={2}>
-                    
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    ></Service>)
+                }
                 </Grid>
             </Container>
         </Wrapper>
@@ -34,47 +36,6 @@ const Services = () => {
     const Wrapper = styled.div`
         margin-top: 100px;
         margin-bottom: 80px;
-    `;
-    const Cards = styled.div`
-        background-color: white;
-        margin: 0px 10px;
-        margin-top:-20px;
-        padding: 20px;
-        > h4 {
-            font-size: 20px;
-        };
-        > p {
-            font-size: 15px;
-        };
-        > button {
-            font-size: 14px;
-            border: 0px solid;
-            display:flex;
-            color:#f68220;
-            background-color: white;
-        };
-        @media only screen and ${breakpoint.device.sm}{
-
-        };
-        @media only screen and ${breakpoint.device.xs}{
-
-        };
-    `;
-    const Image = styled.img`
-        position:relative;
-        z-index:-1;
-        height: 230px;
-        width: 350px;
-        margin-left: -8px;
-    `;
-    const ParentCard = styled.div`
-        margin: 10px 20px;
-        @media only screen and ${breakpoint.device.sm}{
-            
-        };
-        @media only screen and ${breakpoint.device.xs}{
-            
-        };
     `;
 
     const Title = styled.h2`
@@ -89,5 +50,4 @@ const Services = () => {
         font-size: 12px;
         margin-bottom: 30px;
     `
-
 export default Services;
