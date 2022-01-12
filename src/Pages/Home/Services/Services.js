a62e8c91827b34c9a74f8401d5ba2b4b78812721
SamiulKabir <samkabir26@gmail.com>
2022-01-12 11:19:48 +0600

    made this section responsive

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import styled from "styled-components";
-import { useEffect, useState } from 'react';
 import Service from './Service/Service';
 
 const Services = () => {
